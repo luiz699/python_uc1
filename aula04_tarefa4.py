a68dd2685bb452181a05aaa0738e460d1b07f390
luiz699 <LUIZ.VSJ@HOTMAIL.COM>
2025-03-07 21:52:24 -0300

    Add files via upload
    aula04_tarefa4.py

---
+++ aula04_tarefa4.py
@@ -0,0 +1,25 @@
+x=10 
+print(f"o valor de x e {x}")
+
+x+=5
+print(f"o valor de x e {x}")
+
+x-=3
+print(f"o valor de e {x}")
+
+
+x*=2
+print(f"o valor de e {x}")
+
+
+x/=4
+print(f"o valor de e {x}")
+
+
+x//=3
+print(f"o valor de e {x}")
+
+
+x%=2
+print(f"o valor de e {x}")
+
